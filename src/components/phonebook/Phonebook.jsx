8ce97fbb5c1bc00eafd7d4b4c811d5223667a294
AlinaS13 <117362222+AlinaS13@users.noreply.github.com>
2023-03-08 02:24:16 +0200

    fix

--- src/components/phonebook/Phonebook.jsx
+++ src/components/phonebook/Phonebook.jsx
@@ -2,9 +2,9 @@ import { Component } from 'react';
 import { Input } from 'components/input';
 
 export class Phonebook extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
